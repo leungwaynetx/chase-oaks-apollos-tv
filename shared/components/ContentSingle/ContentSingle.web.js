96d036075c80afd8e2a8d0a08e6abed660c62aa0
Jordan Wade <jjwade31@gmail.com>
2021-09-15 12:48:55 -0500

    WIP - Chase oaks (#38)
    * Add Chase Oaks logo
    
    * Initial chase oaks update theme colors
    
    * Add gradient circle around logo

--- shared/components/ContentSingle/ContentSingle.web.js
+++ shared/components/ContentSingle/ContentSingle.web.js
@@ -140,7 +140,18 @@ function ContentSingle(props = {}) {
             display="flex"
             flexDirection="row"
           >
-            <Logo width="28px" mr="base" />
+            <Box
+              borderRadius="999px"
+              backgroundImage="radial-gradient(93.06% 93.06% at 100% 0%, rgba(114, 141, 150, 0.42) 0%, rgba(47, 76, 181, 0) 100%), radial-gradient(83.39% 77.78% at 0% 95.2%, rgba(148, 100, 156, 0.2) 0%, rgba(116, 42, 162, 0) 100%)"
+              backgroundColor="#413A60"
+              alignItems="center"
+              justifyContent="center"
+              width="90px"
+              height="90px"
+              mr="base"
+            >
+              <Logo width="60px" />
+            </Box>
             <Box>
               <SmallBodyText mb="xs">
                 {`To take notes, journal, and more, open the ${'Apollos'} app on your phone.`}
