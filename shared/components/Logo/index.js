5756c800ad37704ca8c4cd6a78c0c63408088239
Jordan Wade <jjwade31@gmail.com>
2021-09-21 14:09:03 -0500

    Fix chase oaks logo viewBox

--- shared/components/Logo/index.js
+++ shared/components/Logo/index.js
@@ -6,7 +6,7 @@ function Logo(props = {}) {
       <svg
         width="100%"
         height="100%"
-        viewBox="0 0 39 40"
+        viewBox="0 0 60 26"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
