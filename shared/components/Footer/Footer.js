88675ea0d6c1c68432cb8f123dc43637706339f4
Ryan Davidson <ryanrkd2112@gmail.com>
2021-09-30 14:30:13 -0400

    Footer update

--- shared/components/Footer/Footer.js
+++ shared/components/Footer/Footer.js
@@ -7,10 +7,10 @@ function Footer(props = {}) {
   return (
     <Box bg="fill.screen" p="xl" pb="l" alignItems="center" {...props}>
       <Box mb="base" opacity="0.3">
-        <Logo width="40px" />
+        <Logo width="60px" />
       </Box>
       <SmallSystemText color="text.secondary">
-        &copy; Copyright information.
+        &copy; 2021 Chase Oaks Church. All Rights Reserved.
       </SmallSystemText>
     </Box>
   );
