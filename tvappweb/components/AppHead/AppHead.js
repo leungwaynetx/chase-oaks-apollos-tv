9342ee388dc4beecf13989166b22091430900e38
Conrad VanLandingham <conrad@differential.com>
2021-10-12 17:01:58 -0500

    fix intercom

--- tvappweb/components/AppHead/AppHead.js
+++ tvappweb/components/AppHead/AppHead.js
@@ -1,40 +1,21 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 
-import { useNavigation } from 'shared/router';
 import amplitude from 'shared/lib/amplitude';
 import { useCurrentUser } from 'shared/hooks';
 
+import { useIntercom } from 'react-use-intercom';
+
 function AppHead() {
   const { currentUser } = useCurrentUser();
-  const router = useNavigation();
-
-  const _isNotBrowser =
-    typeof window === 'undefined' || typeof document === 'undefined';
 
   const fullName =
     currentUser &&
     `${currentUser?.profile?.firstName} ${currentUser?.profile?.lastName}`;
 
-  useEffect(() => {
-    if (_isNotBrowser) return null;
-
-    /* eslint-disable func-names, no-var, vars-on-top, prefer-rest-params */
-    // prettier-ignore
-    return (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/o69wli5a';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-  }, [_isNotBrowser]);
+  const intercom = useIntercom();
 
   useEffect(() => {
-    if (_isNotBrowser) return null;
-
-    return window.Intercom('boot', {
-      app_id: 'o69wli5a',
-    });
-  }, [_isNotBrowser]);
-
-  useEffect(() => {
-    if (_isNotBrowser) return null;
-
     if (fullName || currentUser?.profile?.email) {
       window.Intercom('update', {
         name: fullName, // Full name
@@ -43,25 +24,15 @@ function AppHead() {
       });
     }
 
+    intercom.update({
+      name: fullName,
+      email: currentUser?.profile?.email,
+    });
+
     return null;
-  }, [fullName, currentUser?.profile?.email, _isNotBrowser]);
+  }, [fullName, currentUser?.profile?.email]);
 
   useEffect(() => {
-    if (_isNotBrowser) return null;
-
-    const handleRouteChange = () => {
-      window.Intercom('update');
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events, _isNotBrowser]);
-
-  useEffect(() => {
-    console.log('amplitude use effect');
     // Only run Amplitude Analytics in production
     // if (!process.env.NODE_ENV === 'production') return null;
 
--- tvappweb/pages/_app.js
+++ tvappweb/pages/_app.js
@@ -1,20 +1,25 @@
 import PropTypes from 'prop-types';
 import { AppProvider } from 'shared/providers';
 import '../styles/globals.css';
+import { IntercomProvider } from 'react-use-intercom';
 
 import { Footer, Header } from 'shared/components';
 import { Layout } from 'shared/ui-kit';
 import { AppHead } from '../components';
 
+const INTERCOM_APP_ID = 'o69wli5a';
+
 function App({ Component, pageProps }) {
   return (
     <AppProvider initialApolloState={pageProps.initialApolloState}>
-      <AppHead />
-      <Layout>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </Layout>
+      <IntercomProvider appId={INTERCOM_APP_ID} autoBoot>
+        <AppHead />
+        <Layout>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </Layout>
+      </IntercomProvider>
     </AppProvider>
   );
 }
