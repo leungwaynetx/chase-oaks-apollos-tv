6c355cafc6ce24c6c87caaac08bcff22e441fd23
Conrad VanLandingham <conrad@differential.com>
2021-10-12 16:26:34 -0500

    trying to fix intercom messenger

--- tvappweb/components/AppHead/AppHead.js
+++ tvappweb/components/AppHead/AppHead.js
@@ -9,52 +9,44 @@ function AppHead() {
   const { currentUser } = useCurrentUser();
   const router = useNavigation();
 
-  useEffect(() => {
-    // Do not run Amplitude Analytics unless traffic is coming from a browser
-    const _isNotBrowser =
-      typeof window === 'undefined' || typeof document === 'undefined';
+  const _isNotBrowser =
+    typeof window === 'undefined' || typeof document === 'undefined';
 
+  const fullName =
+    currentUser &&
+    `${currentUser?.profile?.firstName} ${currentUser?.profile?.lastName}`;
+
+  useEffect(() => {
     if (_isNotBrowser) return null;
 
     /* eslint-disable func-names, no-var, vars-on-top, prefer-rest-params */
     // prettier-ignore
     return (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/o69wli5a';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-  });
+  }, [_isNotBrowser]);
 
   useEffect(() => {
-    const _isNotBrowser =
-      typeof window === 'undefined' || typeof document === 'undefined';
-
     if (_isNotBrowser) return null;
 
     return window.Intercom('boot', {
       app_id: 'o69wli5a',
     });
-  });
+  }, [_isNotBrowser]);
 
   useEffect(() => {
-    const _isNotBrowser =
-      typeof window === 'undefined' || typeof document === 'undefined';
-
     if (_isNotBrowser) return null;
 
-    if (currentUser) {
-      const fullName = `${currentUser.profile?.firstName} ${currentUser.profile?.lastName}`;
-
+    if (fullName || currentUser?.profile?.email) {
       window.Intercom('update', {
-        name: currentUser.profile?.nickName || fullName, // Full name
-        email: currentUser.profile?.email, // Email address
+        name: fullName, // Full name
+        email: currentUser?.profile?.email, // Email address
         // created_at: currentUser.created_at, // Signup date as a Unix timestamp
       });
     }
 
     return null;
-  }, [currentUser]);
+  }, [fullName, currentUser?.profile?.email, _isNotBrowser]);
 
   useEffect(() => {
-    const _isNotBrowser =
-      typeof window === 'undefined' || typeof document === 'undefined';
-
     if (_isNotBrowser) return null;
 
     const handleRouteChange = () => {
@@ -66,9 +58,10 @@ function AppHead() {
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [router.events]);
+  }, [router.events, _isNotBrowser]);
 
   useEffect(() => {
+    console.log('amplitude use effect');
     // Only run Amplitude Analytics in production
     // if (!process.env.NODE_ENV === 'production') return null;
 
