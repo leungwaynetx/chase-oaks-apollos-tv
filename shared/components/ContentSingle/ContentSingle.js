187c587b0786bb2d6817adfcbd0803fee6431155
Jordan Wade <jjwade31@gmail.com>
2021-12-01 16:43:37 -0600

    Update design and layout of Native content single/series  (#75)
    * Initial native content single clean up
    
    * Fix edges routing issue on content series
    
    * Fix syntax error
    
    * Add gradient to series backgroundImage

--- shared/components/ContentSingle/ContentSingle.js
+++ shared/components/ContentSingle/ContentSingle.js
@@ -1,21 +1,49 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import format from 'date-fns/format';
-import { Image, Dimensions } from 'react-native';
+import { ImageBackground, Dimensions, FlatList, Animated } from 'react-native';
 import Video from 'react-native-video';
+import { themeGet } from '@styled-system/theme-get';
+import { withTheme } from 'styled-components';
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { apollosPropTypes } from 'shared/lib';
 import { useNavigation } from 'shared/router';
+import { getPathFromType } from 'shared/utils';
 
 import { InteractWhenLoaded, TrackEventWhenLoaded } from 'shared/components';
-import { Box, H2, H4, Loader, utils } from 'shared/ui-kit';
-
-const DEFAULT_CONTENT_WIDTH = utils.rem('1280px');
+import { Box, H2, H4, Loader, ContentItemCard, system } from 'shared/ui-kit';
 
 function ContentSingle(props = {}) {
   const router = useNavigation();
 
   const invalidPage = !props.loading && !props.data;
+  const [scrollViewWidth, setScrollViewWidth] = React.useState(0);
+  const boxWidth = scrollViewWidth * 0.25;
+  const pan = React.useRef(new Animated.ValueXY()).current;
+
+  const StyledFlatList = withTheme(styled(FlatList)`
+    padding-left: ${themeGet('space.xl')};
+    ${system};
+  `);
+
+  const StyledImageBackground = withTheme(styled(ImageBackground)`
+    flex: 1;
+    justify-content: flex-end;
+    aspect-ratio: ${Dimensions.get('window').width /
+    Dimensions.get('window').height};
+    ${system};
+  `);
+
+  const StyledVideo = withTheme(styled(Video)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    ${system};
+  `);
 
   useEffect(() => {
     if (invalidPage) {
@@ -46,14 +74,13 @@ function ContentSingle(props = {}) {
     ? format(new Date(props?.data?.publishDate), 'MMMM do, yyyy')
     : null;
 
-  const handleActionPress = (node) => {
-    router.push(node);
+  const handleActionPress = (item) => {
+    router.push(getPathFromType(item.node), {
+      itemId: item.node.id,
+    });
   };
 
-  const handleGoBack = () => {
-    router.push('/');
-  };
-  let { width, height } = Dimensions.get('window');
+  const { width, height } = Dimensions.get('window');
 
   if (props.data?.videos?.length > 0) {
     return (
@@ -63,24 +90,18 @@ function ContentSingle(props = {}) {
         width={width}
         justifyContent="center"
       >
-        <Video
+        <StyledVideo
           source={{
             uri: props.data?.videos[0]?.sources[0]?.uri,
           }}
           controls
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-          }}
         />
       </Box>
     );
   }
+
   return (
-    <>
+    <Box position="relative">
       <InteractWhenLoaded
         loading={props.loading}
         nodeId={props.data?.id}
@@ -97,29 +118,74 @@ function ContentSingle(props = {}) {
           title,
         }}
       />
-      <Box width="100%" maxWidth={props.contentMaxWidth}>
-        <Box overflow="hidden" height={900} justifyContent="center">
-          <Image
-            style={{
-              aspectRatio:
-                Dimensions.get('window').width /
-                Dimensions.get('window').height,
-            }}
-            resizeMode="cover"
-            source={coverImage?.sources[0]}
-          />
+
+      <StyledImageBackground
+        imageStyle={{
+          bottom: Dimensions.get('window').height / 3,
+          overflow: 'visible',
+        }}
+        source={coverImage?.sources[0]}
+      >
+        <LinearGradient
+          colors={['#00000000', '#1C1C1E']}
+          locations={[0.3, 0.7]}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            paddingBottom: '56.25%',
+          }}
+        />
+        <Box py="s">
+          <Box paddingLeft="xl" mb="xs">
+            {publishDate ? <H4 color="text.secondary">{publishDate}</H4> : null}
+            {title ? <H2>{title}</H2> : null}
+          </Box>
+
+          {edges?.length > 0 ? (
+            <StyledFlatList
+              data={edges}
+              horizontal
+              initialScrollIndex={0}
+              contentContainerStyle={{ paddingVertical: 16 }}
+              refreshing={props.loading}
+              showsHorizontalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <ContentItemCard
+                  image={item.node.coverImage}
+                  title={item.node.title}
+                  onPress={() => handleActionPress(item)}
+                  width={boxWidth}
+                  mx={16}
+                />
+              )}
+              snapToInterval={boxWidth}
+              onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { x: pan.x } } }],
+                {
+                  useNativeDriver: false,
+                }
+              )}
+              keyExtractor={(item) => item.node.id}
+              snapToAlignment={'start'}
+              decelerationRate={'fast'}
+              contentInsetAdjustmentBehavior="never"
+              automaticallyAdjustContentInsets={false}
+              showsVerticalScrollIndicator={false}
+              scrollEventThrottle={1}
+              onLayout={(e) => {
+                setScrollViewWidth(e.nativeEvent.layout.width);
+              }}
+            />
+          ) : null}
         </Box>
-        <Box mb="l">
-          {publishDate ? <H4 color="text.secondary">{publishDate}</H4> : null}
-          {title ? <H2 mb="s">{title}</H2> : null}
-        </Box>
-      </Box>
-    </>
+      </StyledImageBackground>
+    </Box>
   );
 }
 
 ContentSingle.propTypes = {
-  contentMaxWidth: PropTypes.string,
   data: PropTypes.shape({
     childContentItemsConnection: PropTypes.shape(),
     coverImage: PropTypes.shape({}),
@@ -136,10 +202,6 @@ ContentSingle.propTypes = {
     videos: PropTypes.arrayOf(PropTypes.shape({ embedHtml: PropTypes.string })),
   }),
   loading: PropTypes.bool,
-};
-
-ContentSingle.defaultProps = {
-  contentMaxWidth: DEFAULT_CONTENT_WIDTH,
 };
 
 export default ContentSingle;
