977f3111cfa39e64a13923d7d9cd0e6d38df3129
Jordan Wade <jjwade31@gmail.com>
2021-12-10 16:36:52 -0600

    Implement auth (#80)
    * Implement auth on native home screen
    
    * Podfile.lock

--- tvappnative/screens/Home/index.js
+++ tvappnative/screens/Home/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView } from 'react-native';
 
 import { TabFeedProvider } from 'shared/providers';
+import { useAuthState } from 'shared/providers/AuthProvider';
+import { useNavigation } from 'shared/router';
 
 import { FeatureFeed } from 'shared/components';
 // import { FeatureFeedDebugger } from 'shared/components/FeatureFeed';
 import { Box } from 'shared/ui-kit';
 
 const HomeScreen = () => {
+  const router = useNavigation();
+  const { authenticated } = useAuthState();
+
+  useEffect(() => {
+    if (!authenticated) {
+      router.push('/auth');
+    }
+  }, [authenticated, router]);
+
   return (
     <Box backgroundColor="fill.paper">
       <ScrollView flexGrow={1} height="100%">
